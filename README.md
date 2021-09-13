05d99ed891ba442868cc35eebddb747d4205b253
Shandy Maulana Yudantiar <shandy318@gmail.com>
2021-09-13 16:27:51 +0700

    Data Migration

---
+++ README.md
@@ -0,0 +1,16 @@
+Dokumentasi
+
+**DATABASE MIGRATIONS**
+***File** :
+sayurmurah->database->migrations
+
+***Tabel Database**:
+-admin              :id_admin, nama, email, foto, telepon, password, id_akses, status, time_in_user
+-daerah             :id_daerah, nama_daerah, jumlah_pasar
+-detail_transaksi   :id_detail_transaksi, id_transaksi, id_produk, jumlah_transaksi, grand_total
+-jenis_produk       :id_jenis_produk, jenis_produk, deskripsi_jenis_produk, foto_jenis_produk
+-pasar              :id_pasar, id_daerah, nama_pasar, alamat_pasar, foto_pasar
+-produk             :id_produk, id_pasar, nama_produk, id_jenis_produk, harga_produk, deksripsi_produk, foto_produk
+-status_transaksi   :id_status_transaksi, status_transaksi
+-transaksi          :id_transaksi, id_user, id_status_transaksi, tanggal_transaksi, nama_user, alamat_pengiriman, sub_total, total_bayar, total_kembali, bukti_bayar
+-user               :id_user, akses, nama_user, username, email, password, telepon, alamat
--- sayurmurah/database/migrations/2021_09_13_073543_create_pasar_table.php
+++ sayurmurah/database/migrations/2021_09_13_073543_create_pasar_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePasarTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('pasar', function (Blueprint $table) {
+            $table->id('id_pasar')->index();
+            $table->integer('id_daerah');
+            $table->char('nama_pasar')->unique();
+            $table->text('alamat_pasar');
+            $table->char('foto_pasar');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pasar');
     }
 }
