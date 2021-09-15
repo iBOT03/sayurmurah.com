26f2d0747a7a1997d9952cc8ce19a3cc7d56627c
Shandy Maulana Yudantiar <shandy318@gmail.com>
2021-09-15 16:00:38 +0700

    update web.php
    register routes

--- sayurmurah/routes/web.php
+++ sayurmurah/routes/web.php
@@ -24,14 +24,9 @@ Route::get('admin', function () {
 Route::get('admin/akun', function () {
     return view('admin/kelolaakun/akun');
 });
-<<<<<<< Updated upstream
 Route::get('admin/tambahakun', function () {
     return view('admin/kelolaakun/tambahakun');
 });
-Route::get('registrasi', function () {
-    return view('userRegister');
-});
-=======
+
 Route::get('/register', [UserController::class, 'register']);
 Route::get('/login', [UserController::class, 'login']);
->>>>>>> Stashed changes
