919112cf6e9ab1839f55ea10b0c13fe92b9fdd1a
Andrea Santana A <andrea.santana986@gmail.com>
2021-09-16 19:38:17 +0700

    tipis2 gan

--- sayurmurah/routes/web.php
+++ sayurmurah/routes/web.php
@@ -27,6 +27,9 @@ Route::get('admin/akun', function () {
 Route::get('admin/tambahakun', function () {
     return view('admin/kelolaakun/tambahakun');
 });
+Route::get('admin/detailakun', function () {
+    return view('admin/kelolaakun/detailakun');
+});
 
 Route::get('/register', [UserController::class, 'register']);
 Route::get('/login', [UserController::class, 'login']);
